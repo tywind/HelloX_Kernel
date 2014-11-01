ce4613f2968babb73fd8d828cb70ca42f9f24974
garry <garryxin@gmail.com>
2014-11-01 21:46:22 +0800

    HelloX V1.77(Beta) Kernel

--- config/config.h
+++ config/config.h
@@ -23,7 +23,7 @@
 //
 //************************************************************************
 
-#define VERSION_INFO "    HelloX [Version 1.770,build in 2014/10/31,by Garry.Xin]"
+#define VERSION_INFO "    HelloX [Version 1.770(Beta),build in 2014/10/31,by Garry.Xin]"
 #define SLOGAN_INFO  "    HelloX OS,through which you can talk to everything."
 
 //************************************************************************
